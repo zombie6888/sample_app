387a9aebe36bb94267be8b07d9db5f526656d777
zombie6888 <zom688@gmail.com>
2014-05-11 16:05:59 +0400

    add postgres to development

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,9 +1,10 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
   
-  describe "Home page" do
-   
+  describe "Home page" do   
    it "should have the content 'Sample App'" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
       visit '/static_pages/home'
@@ -12,13 +13,12 @@ describe "StaticPages" do
 	
 	it "should have the right title" do
 	  visit '/static_pages/home'
-	  expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+	  expect(page).to have_title("#{base_title} | Home")
 	end
 	
   end 
 
-  describe "Help page" do
-   
+  describe "Help page" do   
    it "should have the content 'Help'" do
       visit '/static_pages/help'
       expect(page).to have_content('Help')
@@ -26,12 +26,11 @@ describe "StaticPages" do
 	
 	it "should have the right title" do
 	  visit '/static_pages/help'
-	  expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")	
+	  expect(page).to have_title("#{base_title} | Help")	
 	end
   end
   
   describe "About page" do
-
     it "should have the content 'About Us'" do
       visit '/static_pages/about'
       expect(page).to have_content('About Us')
@@ -39,11 +38,21 @@ describe "StaticPages" do
 	
 	it "should have the right title" do
 	  visit '/static_pages/about'
-	  expect(page).to have_title("Ruby on Rails Tutorial Sample App | About")
-	end
-	
-  end 
-  
+	  expect(page).to have_title("#{base_title} | About")
+	end	
+  end
+
+  describe "Contact page" do	
+	it "should have the content 'Contact'" do
+	  visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
+   
+	it "should have the right title" do
+	  visit '/static_pages/contact'
+	  expect(page).to have_title("#{base_title} | Contact")
+	end	
+  end  
 end
 
  
